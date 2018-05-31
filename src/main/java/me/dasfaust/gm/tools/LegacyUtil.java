151a6f569aa49714c070a9bfb6c71a6675b3df11
Daniel Saukel <sa.da@unitybox.de>
2018-05-31 18:52:57 +0200

    Fix material name :/

--- src/main/java/me/dasfaust/gm/tools/LegacyUtil.java
+++ src/main/java/me/dasfaust/gm/tools/LegacyUtil.java
@@ -18,7 +18,7 @@ public class LegacyUtil {
     public static final ItemStack BLACK_STAINED_GLASS_PANE = newMats ? new ItemStack(Material.valueOf("BLACK_STAINED_GLASS_PANE")) : new ItemStack(Material.valueOf("STAINED_GLASS_PANE"), 1, (short) 15);
 
     public static final String LIME_STAINED_GLASS_PANE_ID = newMats ? "LIME_STAINED_GLASS_PANE" : "STAINED_GLASS_PANE:5";
-    public static final String RED_STAINED_GLASS_PANE_ID = newMats ? "RED_STAINED_GLASS_PANE" : "RED_GLASS_PANE:14";
+    public static final String RED_STAINED_GLASS_PANE_ID = newMats ? "RED_STAINED_GLASS_PANE" : "STAINED_GLASS_PANE:14";
     public static final String MAP_ID = newMats ? "MAP" : "EMPTY_MAP:0";
 
 }
